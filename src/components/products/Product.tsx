ddf928e7c0d6bcb4dfa859ef0a5a528bbea10ea9
Vuyisa Baza <vuyisa.baza@albatrosgolf.co.za>
2024-09-27 22:01:39 +0200

    Finished styling the product component

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -1,5 +1,5 @@
 import { Product } from '../../types/products.ts';
-
+import './Product.css';
 
 interface ProductProps {
     product: Product
@@ -7,11 +7,16 @@ interface ProductProps {
 
 export default function ProductComponent ({product:{name,description,price,emoji}}: ProductProps) {
   return (
-    <>
-      {emoji}
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>${price}</p>
-    </>
+    <div className='product-container'>
+      <section className='product-image'>
+        <img src={emoji}/>
+      </section>
+      <section className='smythe-regular'>
+        <h3>{name}</h3>
+        <p>{description}</p>
+        <p>${price}</p>
+      </section>
+
+    </div>
   );
 }
--- src/data/products.ts
+++ src/data/products.ts
@@ -1,5 +1,5 @@
 import {Product} from '../types/products.ts';
 
 export const products: Product[] = [
-  {id: 1, name: 'Pizza', description: 'pepperoni,mushrom,mozarella', emoji: '🍕', price: 100},
+  {id: 1, name: 'Pizza', description: 'pepperoni,mushrom,mozarella', emoji: 'https://plus.unsplash.com/premium_photo-1674147605295-53b30e11d8c0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', price: 100},
 ];
